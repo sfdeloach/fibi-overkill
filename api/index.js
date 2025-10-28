0346309e46acc9bbc5f8c6f794a1cc68a5afc5e3
sfdeloach <sfdeloach@gmail.com>
2025-10-28 00:02:46 -0400

    postgres breaking

--- api/index.js
+++ api/index.js
@@ -1,18 +1,43 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-
+// environment variables
 const env = require("./env");
 const port = env.API_PORT;
+const postgresHost = env.POSTGRES_HOST;
+const postgresPort = env.POSTGRES_PORT;
+const postgresUser = env.POSTGRES_USER;
+const postgresDatabase = env.POSTGRES_DATABASE;
+const postgresPassword = env.POSTGRES_PASSWORD;
 
-const dummyPostgresData = [
-  { index: 0, date: "2024-01-01T12:00:00Z" },
-  { index: 8, date: "2024-01-02T13:00:00Z" },
-  { index: 9, date: "2024-01-03T14:00:00Z" },
-  { index: 10, date: "2024-01-04T15:00:00Z" },
-];
+// express setup
+const express = require("express");
+const app = express();
+
+// body parser middleware for json
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// postgres client setup
+const pg = require("pg");
+const pool = new pg.Pool({
+  host: postgresHost,
+  port: postgresPort,
+  user: postgresUser,
+  database: postgresDatabase,
+  password: postgresPassword,
+});
+
+// create tables if not exist
+const createTableText = `
+CREATE TABLE IF NOT EXISTS indexes (
+  _id SERIAL PRIMARY KEY,
+  index INT,
+  date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+`;
+
+pool
+  .query(createTableText)
+  .query("INSERT INTO indexes (index) VALUES ($1)", [3]) // remove after testing
+  .catch((err) => console.error("Error occurred during pool query: ", err));
 
 const dummyRedisData = [
   { key: 0, value: 0 },
@@ -21,13 +46,22 @@ const dummyRedisData = [
   { key: 10, value: 55 },
 ];
 
-// Routes
+// route handlers
 app.get("/", (req, res) => {
   res.send("Hello ExpressJS!");
 });
 
 app.get("/indexes", (req, res) => {
-  res.json(dummyPostgresData);
+  const queryText = "SELECT * FROM indexes;";
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.json(result.rows);
+    })
+    .catch((err) => {
+      console.error("Error querying indexes", err);
+      res.status(500).send("Error retrieving indexes");
+    });
 });
 
 app.get("/values", (req, res) => {
@@ -40,11 +74,12 @@ app.post("/index", (req, res) => {
       index: req.body.index,
       date: new Date().toISOString(),
     });
-    dummyRedisData.push({ key: req.body.index, value: -1 }); // Placeholder value
+    dummyRedisData.push({ key: req.body.index, value: -1 }); // placeholder value
     res.send("OK");
-  }, 3000); // Simulate processing delay
+  }, 3000); // simulate processing delay
 });
 
+// start the server
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
